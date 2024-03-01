1f301afbae53dfff4d2558c164fdd8987a1c350b
Abhishek Dubey <70766823+abhishekopd@users.noreply.github.com>
2024-03-01 16:39:36 +0530

    adding loc

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
-  location            = azurerm_resource_group.location
+  location            = var.nsg_location
   resource_group_name = var.resource_group_name
 }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,4 @@
 nsg_rules = "./entp-ad01-nsg-rules.json"
 nsg_name = "entp-ad01-dev"
 resource_group_name = "APIM-RG"
+nsg_location = "Central India"
