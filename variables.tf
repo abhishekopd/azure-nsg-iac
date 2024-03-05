af25ecc1ce80a5b16dac98976b67aaa5c6749245
Abhishek Dubey <70766823+abhishekopd@users.noreply.github.com>
2024-03-05 15:03:30 +0530

    aks nsg

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "nsg_rules" {
-  description = "The path to the JSON or YAML file containing the NSG rules"
-  type        = string
-}
+#variable "nsg_rules" {
+#  description = "The path to the JSON or YAML file containing the NSG rules"
+#  type        = string
+#}
 
 variable "nsg_name" {
   description = "The name of the NSG to deploy the rules to"
